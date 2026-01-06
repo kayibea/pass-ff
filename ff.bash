2bea672e679d13c99d943d090190dcdfa777b203
kayibea <kayibea@proton.me>
2026-01-07 00:56:54 +0100

    refactor: improve files search logic

--- ff.bash
+++ ff.bash
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-readonly VERSION="0.0.1"
+store="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
+
+cd "$store"
 
 command -v fzf >/dev/null 2>&1 || {
   echo "fzf not found, aborting." >&2
   exit 1
 }
-
-IGNORE_DIRS=(
-  ".git"
-  ".extensions"
-  ".trash"
-)
-
-prune_expr=()
-for dir in "${IGNORE_DIRS[@]}"; do
-  prune_expr+=(-path "*/$dir/*" -o)
-done
-unset 'prune_expr[${#prune_expr[@]}-1]' # remove trailing -o
 
 query=""
 pass_args=()
@@ -38,11 +28,10 @@ if [[ $# -gt 0 ]]; then
   done
 fi
 
-file="$(find "$PREFIX" \
-  \( "${prune_expr[@]}" \) -prune -o \
-  -type f -name '*.gpg' -printf '%P\n' |
-  sed 's/\.gpg$//' |
-  fzf -e --query="$query")"
+file="$(
+  find . -type f -name '*.gpg' -not -path '*/.*' -printf '%P\n' |
+    sed 's/\.gpg$//' | fzf -e --query="$query"
+)"
 
 [[ -z "$file" ]] && exit 0
 
